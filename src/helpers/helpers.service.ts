35054940fab27146b8febe530f920bc337e712cf
sheilamr <sheila.24.1996@gmail.com>
2026-03-10 12:00:53 +0100

    upd

--- src/helpers/helpers.service.ts
+++ src/helpers/helpers.service.ts
@@ -135,35 +135,4 @@ export class helpersService {
             Number(ss)
         );
     }
-
-    normalizeNIF(nif: string): string {
-        if (!nif) return '';
-        // Limpiar espacios y pasar a mayúsculas
-        let normalized = nif.trim().toUpperCase().replace(/[^A-Z0-9]/g, '');
-
-        // Transformamos los patrones usando letra = [A-Z] y número = \d
-        const patterns = [
-            /^ES\d{8}[A-Z]$/,   // ES########@
-            /^\d{8}[A-Z]$/,     // ########@
-            /^ES[A-Z]\d{8}$/,   // ES@########
-            /^[A-Z]\d{8}$/,     // @########
-            /^ES[A-Z]\d{7}[A-Z]$/, // ES@#######@
-            /^[A-Z]\d{7}[A-Z]$/,   // @#######@
-            /^[A-Z]\d{8}[A-Z]$/,   // @########@
-            /^[A-Z]\d{6}[A-Z]$/,   // @######@
-            /^[A-Z]\d{5}[A-Z]$/,   // @#####@
-            /^\d{7}[A-Z]$/,     // #######@
-            /^\d{6}[A-Z]$/,     // ######@
-            /^\d{5}[A-Z]$/,     // #####@
-            /^[A-Z]\d{7}$/,     // @#######
-            /^[A-Z]\d{6}$/,     // @######
-            /^[A-Z]\d{5}$/      // @#####
-        ];
-
-        for (const pattern of patterns) {
-            if (pattern.test(normalized)) return normalized;
-        }
-
-        throw new Error(`NIF inválido para BC: ${nif}`);
-    }
 }
