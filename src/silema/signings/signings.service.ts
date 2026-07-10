fb9f4a4f4dfa6f1dc3109db78d64ffdca2b8bfa7
sheilamr <sheila.24.1996@gmail.com>
2026-07-10 10:59:57 +0200

    upd

--- src/silema/signings/signings.service.ts
+++ src/silema/signings/signings.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { getTokenService } from 'src/connection/getToken.service';
 import { runSqlService } from 'src/connection/sqlConnection.service';
 import axios from 'axios';
-import { response } from 'express';
 import * as mqtt from 'mqtt';
 import * as moment from 'moment-timezone';
 
@@ -13,6 +12,7 @@ export class signingsService {
     username: process.env.MQTT_USER,
     password: process.env.MQTT_PASSWORD,
   });
+
   constructor(
     private token: getTokenService,
     private sql: runSqlService,
@@ -22,21 +22,41 @@ export class signingsService {
     let signings;
     try {
       signings = await this.sql.runSql(
-        `select convert(nvarchar, tmst, 121) tmstStr, idr, tmst, accio, usuari, (select nom from dependentes where codi = usuari) as nombre, isnull((SELECT valor FROM dependentesExtes WHERE id = usuari AND nom = 'DNI'), '') as dni, isnull(editor, '') editor, isnull(left(historial, 100), '') historial, isnull(lloc, '') lloc, isnull(left(comentari, 50), '') comentari, id from cdpDadesFichador where tmst>=(select timestamp from records where concepte='BC_CdpDadesFichador') and comentari not like '%365EquipoDeTrabajo%' and year(tmst)<=year(getdate()) and tmst<= GETDATE() order by tmst`,
+        `select 
+          convert(nvarchar, tmst, 121) tmstStr, 
+          idr, 
+          tmst, 
+          accio, 
+          usuari, 
+          (select TOP 1 nom from dependentes where codi = usuari) as nombre, 
+          isnull((SELECT TOP 1 valor FROM dependentesExtes WHERE id = usuari AND nom = 'DNI'), '') as dni, 
+          isnull(editor, '') editor, 
+          isnull(left(historial, 100), '') historial, 
+          isnull(lloc, '') lloc, 
+          isnull(left(comentari, 50), '') comentari, 
+          id 
+        from cdpDadesFichador 
+        where tmst >= (select max(timestamp) from records where concepte='BC_CdpDadesFichador') 
+          and comentari not like '%365EquipoDeTrabajo%' 
+          and year(tmst) <= year(getdate()) 
+          and tmst <= GETDATE() 
+        order by tmst`,
         database,
       );
     } catch (error) {
       this.logError(`❌ Error al ejecutar la consulta SQL en la base de datos '${database}'`, error);
       return false;
     }
+
     if (signings.recordset.length == 0) {
-      //Comprovacion de errores y envios a mqtt
       this.client.publish('/Hit/Serveis/Apicultor/Log', 'No hay registros');
       console.warn('⚠️ Advertencia: No se encontraron registros de fichajes en la base de datos');
       return false;
     }
+
     const token = await this.token.getToken2(client_id, client_secret, tenant);
     let i = 1;
+
     for (const signing of signings.recordset) {
       let timestamp = moment.utc(signing.tmst).tz("Europe/Madrid", true).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
       try {
@@ -53,6 +73,7 @@ export class signingsService {
           comentari: signing.comentari,
           id: signing.id,
         };
+
         let res;
         try {
           res = await axios.get(`${process.env.baseURL}/v2.0/${tenant}/${entorno}/ODataV4/Company('${companyNAME}')/cdpDadesFichador2?$filter=idr eq '${signing.idr}'`, {
